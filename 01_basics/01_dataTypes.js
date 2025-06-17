65a8131ae5a0ea00896979c107dcf94b1602e748
Raafia Fatima <raafiafatima5@gmail.com>
2025-06-17 08:46:50 +0500

    data types and variable

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,17 @@
+// operations: after data conversions
+
+// adding or subtracting numbers or strings
+
+"use strict"; // treat all JS code with newer standard
+
+//alert(3 + 3); // wont run because we are not in browser, we are in node.js
+// tc39.es: standard docs for js  
+let num = 3;
+let neg = -num;
+console.log(neg);
+console.log("nothing");
+
+// types of data tyoes 
+// number, bigint, string, boolean, null (representation of empty value), undefined (a value is not defined), sumbol (used to define uniquness)
+// null = tyoe of : object 
+// undefined : tyoe of: undefined 
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accoundId = 144565
+let accountEmail = "hitesh@google.com"
+var password = "12345"
+city = "jaipur"
+
+// accoundId = 2 
+
+console.log(accoundId);
+console.table([accoundId, accountEmail, password, city])
+
