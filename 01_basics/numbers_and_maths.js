65385ffdfcbcdb2d3f3ee1c5c2aad0e6c67ebb8e
Raafia Fatima <raafia.fatima.2004@gmail.com>
2025-07-09 17:41:50 +0500

    Numbers and Maths

---
+++ 01_basics/numbers_and_maths.js
@@ -0,0 +1,10 @@
+//numbers and maths 
+
+let num = 2000
+let balance = new Number(100.01)
+
+console.log(num, balance)
+
+console.log(balance.toFixed(3)) // adds specified number of decimal after the number
+
+// i am going to learn everything 
