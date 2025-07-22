e7cb018ee2e351c409ef44bdd6fcc99b963012cc
Raafia Fatima <raafia.fatima.2004@gmail.com>
2025-07-22 23:31:12 +0500

    Static

--- 10_Object_Oriented/staticProp.js
+++ 10_Object_Oriented/staticProp.js
@@ -1,2 +1,27 @@
 // prop = properties 
 
+class User{
+    constructor(name){
+        this.name = name 
+    }
+    logMe(){
+        console.log(`The name is ${this.name}`)
+    }
+    // you want the inherited class and the original class to not have this method  
+    static creatId(){
+        return `123`
+    }
+}
+
+class Teacher extends User{
+    constructor(name, email){
+        super(name)
+        this.email = email
+    }
+}
+const raafia = new User('raafia')
+// console.log(raafia.creatId())
+
+const iphone = new Teacher('iphone', 'i@phone.com')
+iphone.logMe()
+console.log(iphone.creatId())
